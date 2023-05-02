958c20778dc4e6fd4b086e41f8612b4902172204
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-05-02 11:15:34 +0200

    more adjusting responsive

--- pages/index.js
+++ pages/index.js
@@ -15,25 +15,25 @@ export default function Home() {
     <main className={`flex min-h-screen items-center bg-red-200`}
     >
       <div>
-        <h1 className="mt-10 text-4xl tracking-widest text-center text-gray-400">Her kommer snart Hjertetings nettbutikk</h1>
+        <h1 className="text-4xl leading-10 tracking-widest text-center text-gray-400 mt-14 lg:mt-10">Her kommer snart Hjertetings nettbutikk</h1>
         <Image src={svg2} className="mx-auto mt-10"
         width={50}
         alt="stort hjerte illustrasjon" />
 
         <div className="grid grid-cols-1 mt-4 place-items-center sm:grid-cols-2 lg:grid-cols-4">
-          <Image src={img1} className='w-1/2 mt-6 border-4 border-white border-double'
+          <Image src={img1} className='w-4/5 mt-6 border-4 border-white border-double lg:w-1/2'
                         width={435}
                         alt='Bilde av en bunke med sovemasker' />
 
-          <Image src={img2} className="w-1/2 mt-6 border-4 border-white border-double" 
+          <Image src={img2} className="w-4/5 mt-6 border-4 border-white border-double lg:w-1/2" 
           width={435} 
           alt="Bilde av eric med kaningutt" />
 
-          <Image src={img3} className="w-1/2 mt-6 border-4 border-white border-double" 
+          <Image src={img3} className="w-4/5 mt-6 border-4 border-white border-double lg:w-1/2" 
           width={435} 
           alt="Bilde av theo med pannebånd" />
           
-          <Image src={img4} className="w-1/2 mt-6 border-4 border-white border-double" 
+          <Image src={img4} className="w-4/5 mt-6 border-4 border-white border-double lg:w-1/2" 
           width={435} 
           alt="Bilde av bunke med pannebånd" />
 
@@ -43,7 +43,7 @@ export default function Home() {
                         width={435}
                         alt='svg naal og traad' />
 
-          <h2 className="text-base tracking-widest text-center text-gray-400 md:text-xl lg:text-2xl">handlaga for deg...</h2>
+          <h2 className="mt-4 mb-10 text-xl tracking-widest text-center text-gray-400 md:text-xl lg:text-2xl lg:mb-0">handlaga for deg...</h2>
       </div>
 
       
